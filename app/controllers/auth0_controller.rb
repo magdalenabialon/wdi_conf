67a1f0b1dca72d40ed7eb2b1e84db594b0ec409c
Maggie McCain <maggie.mccain13@gmail.com>
2016-09-07 16:01:08 +1000

    included sessions and removed email validation

--- app/controllers/auth0_controller.rb
+++ app/controllers/auth0_controller.rb
@@ -1,30 +1,21 @@
 class Auth0Controller < ApplicationController
   def callback
      # example request.env['omniauth.auth'] in https://github.com/auth0/omniauth-auth0#auth-hash
-    # id_token = session[:userinfo]['credentials']['id_token']
-    # store the user profile in session and redirect to root
-    # session[:userinfo] = request.env['omniauth.auth']
-    @loginUser = User.find_by(email: request.env['omniauth.auth']["info"]["email"])
-    if @loginUser
-      @user = @loginUser
-      render "dashboard/show.html.erb"
+    if @loginUser = User.find_by(image_url: request.env['omniauth.auth']["info"]["image"])
+      session[:userinfo] = @loginUser
+      redirect_to "/"
     else
-      if request.env['omniauth.auth']["info"]["email"] == nil
-        render "dashboard/error.html.erb"
+      @user = User.new
+      @user.name = request.env['omniauth.auth']["info"]["name"]
+      @user.email = request.env['omniauth.auth']["info"]["email"]
+      @user.image_url = request.env['omniauth.auth']["info"]["image"]
+      if @user.save
+        session[:userinfo] = @user
+        redirect_to "/"
       else
-        @user = User.new
-        @user.name = request.env['omniauth.auth']["info"]["name"]
-        @user.email = request.env['omniauth.auth']["info"]["email"]
-        @user.image_url = request.env['omniauth.auth']["info"]["image"]
-        if @user.save
-          # render "dashboard/show.html.erb"
-          redirect_to '/'
-        else
-          render "dashboard/error.html.erb"
-        end
+        render "dashboard/error.html.erb"
       end
     end
-    # render "dashboard/show.html.erb"
   end
 
   def failure
--- config/routes.rb
+++ config/routes.rb
@@ -28,10 +28,3 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-
-# # getting the login form
-# get '/login', to: 'session#new'
-# # creating a session / logging in
-# post '/session', to: 'session#create'
-# # destroying a session / logging out
-# delete '/session', to: 'session#destroy'
